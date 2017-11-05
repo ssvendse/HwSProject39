dc0c3451b3cae9b8995da604411468f246c2112f
Skyler Svendsen <skylersvendsen@gmail.com>
2017-11-04 23:46:01 -0500

    first commit

---
+++ HwSProject39/PlayData.swift
@@ -0,0 +1,9 @@
+//
+//  PlayData.swift
+//  HwSProject39
+//
+//  Created by Skyler Svendsen on 11/4/17.
+//  Copyright © 2017 Skyler Svendsen. All rights reserved.
+//
+
+import Foundation
---
+++ README.md
@@ -0,0 +1,1 @@
+# HwSProject39
